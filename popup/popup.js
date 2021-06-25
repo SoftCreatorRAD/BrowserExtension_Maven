6a18f4b1c041377f4bd51cd6d3cc55234a944a05
CuriousLynx <box2288@yandex.ru>
2021-06-25 22:23:06 +0300

    created templates for popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,25 @@
-document.querySelector('.login-btn').addEventListener('click', function () {
-  location = "main.html"
+var isLogined = false;
+
+let popupContent = document.querySelector('.popup-content');
+let loginTemplate = document.querySelector('#login-template');
+let mainTemplate = document.querySelector('#main-template');
+
+
+document.addEventListener('mousedown', function (evt) {
+  if (evt.target.classList.contains('link')) {
+    chrome.tabs.create({url: evt.target.dataset.link, active: true});
+    window.close();
+  }
+  if (evt.target.classList.contains('login-btn')) {
+    isLogined = true;
+    popupContent.innerHTML = '';
+    popupContent.appendChild(mainTemplate.content.firstElementChild.cloneNode(true));
+  }
 });
+
+
+if (isLogined) {
+  popupContent.appendChild(mainTemplate.content.firstElementChild.cloneNode(true));
+} else {
+  popupContent.appendChild(loginTemplate.content.firstElementChild.cloneNode(true));
+}
